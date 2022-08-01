919c3d855fa6a83332df00c4d73f30b21038c7a9
pblgllgs <pbl.gllgs@gmail.com>
2022-07-31 21:30:35 -0400

    properties types rent and sale

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,8 @@
 import { Box } from '@chakra-ui/react';
 import Head from 'next/head';
 import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
     return (
@@ -9,9 +11,9 @@ const Layout = ({ children }) => {
                 <title>Real Estate</title>
             </Head>
             <Box maxWidth="1280px" m="auto">
-                <header>Navbar</header>
+                <header><Navbar /></header>
                 <main>{children}</main>
-                <footer>Footer</footer>
+                <footer><Footer /></footer>
             </Box>
         </>
     );
---
+++ components/SearchFilters.jsx
@@ -0,0 +1,45 @@
+import { Box, Flex, Select } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { filterData, getFilterValues } from '../utils/filterData';
+
+const SearchFilters = () => {
+    const [filters, setFilters] = useState(filterData);
+    const router = useRouter();
+    const SearchProperties = (filterValues) => {
+        const path = router.pathname;
+        const { query } = router;
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            query[item.name] = item.value;
+        });
+
+        router.push({pathname: path, query});
+    };
+    return (
+        <Flex bg="gray.100" p="4" justifyContent="center">
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        onChange={(e) =>
+                            SearchProperties({
+                                [filter.queryName]: e.target.value,
+                            })
+                        }
+                    >
+                        {filter?.items?.map((item) => (
+                            <option key={item.value} value={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
+    );
+};
+
+export default SearchFilters;
